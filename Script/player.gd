8328c4d3e23c961547c811a2613d485125630bb9
Missclick <gabrielgarrone670gmail.com>
2024-12-07 12:26:27 +0100

    Update hurted timer functionality

--- Script/player.gd
+++ Script/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var animation : AnimatedSprite2D = $AnimatedSprite2D
 @onready var invincibility_timer: Timer = $Invincibility
+@onready var hurted_timer: Timer = $Hurted
 
 @export var speed: int = 250
 @export var experience: int = 0
@@ -13,6 +14,11 @@ var death_animation_played : bool = false
 var immortal: bool = false
 var invincible: bool = false
 
+func play_animation(animation_name: String) -> void:
+	if not alive:
+		return
+	animation.play(animation_name)
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
@@ -20,22 +26,22 @@ func get_input():
 func _input(event):
 	if alive:
 		if Input.is_key_pressed(KEY_Z) or Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_DOWN):
-			animation.play("walk_shadow")
+			play_animation("walk_shadow")
 		elif Input.is_key_pressed(KEY_RIGHT) or Input.is_key_pressed(KEY_D):
-			animation.play("walk_shadow")
+			play_animation("walk_shadow")
 			animation.scale.x = abs(animation.scale.x)
 		elif Input.is_key_pressed(KEY_Q) or Input.is_key_pressed(KEY_LEFT):
-			animation.play("walk_shadow")
+			play_animation("walk_shadow")
 			animation.scale.x = -abs(animation.scale.x)
 		else:
-			animation.play("idle_shadow")
+			play_animation("idle_shadow")
 
 func check_health():
 	if immortal:
 		return
 	if health <= 0 and not death_animation_played:
 		alive = false
-		animation.play("death")
+		play_animation("death")
 		death_animation_played = true
 
 func take_damage(enemyVelocity, knockback_force, damage):
@@ -48,10 +54,11 @@ func take_damage(enemyVelocity, knockback_force, damage):
 func enemy_attack(velocity, knockback_force, damage):
 	if alive and not invincible:
 		take_damage(velocity, knockback_force, damage)
-		animation.play("hurt")
+		play_animation("hurt")
 		check_health()
 		invincible = true
 		invincibility_timer.start()
+		hurted_timer.start()
 
 func _physics_process(delta):
 	if alive:
@@ -63,3 +70,5 @@ func _on_invincibility_timeout() -> void:
 
 func _on_hurted_timeout() -> void:
 	animation.stop()
+	play_animation("idle_shadow")
+	
